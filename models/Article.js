03764fd63c1369e486b57fc61126e8efd474c89b
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-09 13:21:18 -0700

    Added link to handdlebars

--- models/Article.js
+++ models/Article.js
@@ -24,8 +24,10 @@ var ArticleSchema = new Schema({
         type: Boolean,
         default: false
     },
-    note: 
-        []
+    note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 
 });
 
