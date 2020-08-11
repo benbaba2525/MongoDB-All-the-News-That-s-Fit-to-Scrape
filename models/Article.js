a57f481034792c8daf613b7e84c871990c3dbe54
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-10 18:57:14 -0700

    Link articles scraper to html page

--- models/Article.js
+++ models/Article.js
@@ -8,7 +8,7 @@ var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
 
-    title: {
+    /*title: {
         type : String,
         required: true
     },
@@ -29,7 +29,28 @@ var ArticleSchema = new Schema({
     ref: "Note"
   }
 
-});
+});*/
+
+
+    // `title` is required and of type String
+    title: {
+      type: String,
+      required: true
+    },
+    // `link` is required and of type String
+    link: {
+      type: String,
+      required: true
+    },
+    // `note` is an object that stores a Note id
+    // The ref property links the ObjectId to the Note model
+    // This allows us to populate the Article with an associated Note
+    note: {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }
+  });
+  
 
 // This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
