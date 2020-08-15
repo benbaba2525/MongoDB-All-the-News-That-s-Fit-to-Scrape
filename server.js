ddb646b6e1b2805c70bc94fee2736c4e62c4b8a9
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-15 01:13:02 -0700

    Edit code to make note show message

--- server.js
+++ server.js
@@ -88,24 +88,19 @@ app.delete("/delete/:id", function(req, res) {
 });
 
 app.put("/newnote/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-  .then(function(dbNote) {
-    // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-    // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-    // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-  })
-  .then(function(dbArticle) {
-    // If we were able to successfully update an Article, send it back to the client
-    res.json(dbArticle);
-  })
-  .catch(function(err) {
-    // If an error occurred, send it to the client
-    res.json(err);
-  });
+    console.log("**********************************")
+    console.log(req.body)
+    console.log(req.body._id);
+    console.log(req.body.note);
+    db.Article.updateOne({ _id: req.body._id }, { $push: { note: req.body.note }}, function(err, result) {
+        console.log(result)
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        } 
+    })
 });
-
 
 
 
