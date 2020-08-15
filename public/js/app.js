848e16561e5ccaaa748edd2106027f9a3d200a9e
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-14 23:53:48 -0700

    moved javascript into js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,72 @@
-// $(document).ready(function () {
 
-//     $("#scrapeBtn").on("click", function (event) {
-//         event.preventDefault();
-//         $.get("/newscrape", function (data) {
-//             location.reload();
-//         });
-//     });
+// Index Page
+$(document).ready(function () {
 
-//     $("#id").on("click", function (event) {
-//         event.preventDefault();
-//         console.log("save it!");
-//         var id = $(this).val();
-       
-//         console.log(id);
-//         $.put("/update/" + id, function(data) {
-//             console.log(data)
-//             location.reload();
-//         })
-//     });
-// });
+    $("#scrapeBtn").on("click", function (event) {
+        event.preventDefault();
+        $.get("/newscrape", function (data) {
+            location.reload();
+             
+        });
+         alert("=== SCRAPED NEW ARTICLES !! ===");
+    });
 
+    $("#save-article").on("click", function (event) {
+        event.preventDefault();
+        console.log("save it!");
+        var id = $(this).children().val();
+        console.log(id);
+        var data = {
+            _id: id
+        }
+        $.ajax("/update/" + id, {
+            type: "PUT",
+            data: data
+        })
+        location.reload();
+        alert("=== SUCESSFULLY SAVED !! ===");
+    });
 
+});
 
+// Saved Page
 
+$(document).ready(function () {
+    $("#new-note").on("submit", function (event) {
+        event.preventDefault();
+        console.log("new note");
+        var id = $(this).children().val();
+        console.log(id);
+        var newNote = $(this).children("#message").val();
+        console.log(id);
+        console.log(newNote);
+        var data = {
+            _id: id,
+            note: newNote
+        }
+        console.log(data);
+        $.ajax({
+            type: "PUT",
+            url: "/newnote/" + id,
+            data: data
+        })
+        location.reload();
+        $("#message").val("");
+    });
+    $("#delete-article").on("click", function (event) {
+        event.preventDefault();
+        console.log("delete it!");
+        var id = $(this).children().val();
+        console.log(id);
+        var data = {
+            _id: id
+        }
+        $.ajax("/unsave/" + id, {
+            type: "PUT",
+            data: data
+        })
+        location.reload();
+        alert("=== REMOVED ARTICLE FROM SAVED!! ===");
+    });
 
-
-
+});
